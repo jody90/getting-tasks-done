ca2b796f30f6e302fe833cb5b661a4789e60c9ac
Jody <johannes.lerch@buttinette.de>
2018-05-21 17:29:12 +0200

    eslint rule for identions changed. HelloWorld removed

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Getting Tasks Done!</h1>
+        <h3>Dashboard</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Dashboard',
+    data () {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
